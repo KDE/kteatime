d90d208bfb9b239759df44c6b80324d10cddac11
Stephan Kulow <coolo@kde.org>
2000-11-21 11:56:33 +0000

    small fixes
    svn path=/trunk/kdetoys/kteatime/; revision=71608

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include "toplevel.h"
 
 
-static const char *description = 
+static const char *description =
 	I18N_NOOP("KDE utility for making fine cup of tea");
 
 static const char *version = "v0.0.1";
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     "(c) 1998-1999, Matthias Hoelzer-Kluepfel");
   aboutData.addAuthor("Matthias Hoelzer-Kluepfel",0, "hoelzer@kde.org");
   KCmdLineArgs::init( argc, argv, &aboutData );
-  
+
   KApplication app;
 
   TopLevel toplevel;
@@ -37,6 +37,7 @@ int main(int argc, char *argv[])
   toplevel.show();
 
   app.setTopWidget(&toplevel);
+  KWin::appStarted();
 
   return app.exec();
 }
