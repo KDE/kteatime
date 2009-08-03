e448d6eda6df0d200433d858e1fbfd54bb15cb6c
Stefan Böhmann <kde@hilefoks.org>
2009-08-03 17:12:58 +0000

    bump version to 1.2.1
    svn path=/trunk/KDE/kdetoys/kteatime/; revision=1006393

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@
 
 
 static const char description[] = I18N_NOOP("KDE utility for making a fine cup of tea.");
-static const char version[] = "v1.2.0";
+static const char version[] = "v1.2.1";
 
 
 int main (int argc, char *argv[])
