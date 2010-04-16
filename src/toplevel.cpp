8cb180a167ce94317e31665b0b14499976a14dc4
Stefan Böhmann <kde@hilefoks.org>
2010-04-16 11:22:17 +0000

    update/unify licence header
    svn path=/trunk/KDE/kdetoys/kteatime/; revision=1115401

--- src/toplevel.cpp
+++ src/toplevel.cpp
@@ -1,23 +1,20 @@
 /*
- *   This file is part of the KTeaTime application.
+ * Copyright 1998-1999 by Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
+ * Copyright 2002-2003 by Martin Willers <willers@xm-arts.de>
+ * Copyright 2007-2009 by Stefan Böhmann <kde@hilefoks.org>
  *
- *   Copyright (C) 1998-1999  Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
- *   Copyright (C) 2002-2003  Martin Willers <willers@xm-arts.de>
- *   Copyright (c) 2007-2009  Stefan Böhmann <kde@hilefoks.org>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "toplevel.h"
 #include "timeedit.h"
