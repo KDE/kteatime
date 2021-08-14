a2cef2a9806a0832bdcaf9f70579b1f6157b6d37
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-14 10:10:05 +0200

    Convert license headers to SPDX

--- src/toplevel.cpp
+++ src/toplevel.cpp
@@ -1,21 +1,10 @@
 /*
- * Copyright 1998-1999 by Matthias Hölzer-Klüpfel <matthias@hoelzer-kluepfel.de>
- * Copyright 2002-2003 by Martin Willers <willers@xm-arts.de>
- * Copyright 2007-2009 by Stefan Böhmann <kde@hilefoks.org>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    SPDX-FileCopyrightText: 1998-1999 Matthias Hölzer-Klüpfel <matthias@hoelzer-kluepfel.de>
+    SPDX-FileCopyrightText: 2002-2003 Martin Willers <willers@xm-arts.de>
+    SPDX-FileCopyrightText: 2007-2009 Stefan Böhmann <kde@hilefoks.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 #include "toplevel.h"
 #include "timeedit.h"
 #include "settings.h"
